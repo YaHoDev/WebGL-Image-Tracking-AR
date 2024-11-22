10afacf5db2001a771a814723e924ed1758b92c9
Yaroslav Homziak <yaroslav.homziak@adv.ua>
2024-11-22 16:57:30 +0200

    chore (script update)

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let params = new URLSearchParams(document.location.search);
-const  contentid= params.get("contentid");
+const my_target_image = params.get("contentid");
 const targetImg = document.querySelector("#my_target_image")
-targetImg.src = `targets/${contentid}`
+targetImg.src = `targets/${my_target_image}.png`
+
+console.log(my_target_image)
+console.log(targetImg)
