d3e4c67c42d985ae36612b4005f6418532ea5133
Yaroslav Homziak <yaroslav.homziak@adv.ua>
2024-11-25 11:36:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const  contentid = params.get("contentid");
 const targetImg = document.querySelector("#my_target_image")
 targetImg.src = ` https://oldpressobjects.s3.eu-north-1.amazonaws.com/${contentid}/my_target_image `
 
-console.log(my_target_image)
-console.log(targetImg)
+console.log("content ID: " + contentid)
+console.log("targetIMG: " + targetImg)
+console.log("New src: " + targetImg.src)
